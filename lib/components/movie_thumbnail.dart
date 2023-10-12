7a9a05c882fe0b68be74b06f5d3f6e7d1349416f
AlvinMrema <sonalpha023@gmail.com>
2023-10-12 12:51:30 +0300

    Fixed CachedNetworkImage issues

--- lib/components/movie_thumbnail.dart
+++ lib/components/movie_thumbnail.dart
@@ -17,35 +17,44 @@ class MovieThumbnail extends StatelessWidget {
     final movieThumbnailUrl = Service.thumbnailRootPath + movie.posterPath;
 
     return Container(
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Expanded(
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(12),
+      margin: const EdgeInsets.all(20),
+      height: 400,
+      decoration: const BoxDecoration(
+        color: Colors.grey,
+        borderRadius: BorderRadius.all(
+          Radius.circular(12),
+        ),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
               child: CachedNetworkImage(
                 imageUrl: movieThumbnailUrl,
                 progressIndicatorBuilder: (context, url, downloadProgress) =>
                     CircularProgressIndicator(value: downloadProgress.progress),
                 errorWidget: (context, url, error) => const Icon(Icons.error),
+                memCacheHeight: 300, //this line
                 fit: BoxFit.cover,
-                width: 160,
+                width: 200,
                 height: 300,
               ),
             ),
-          ),
-          const SizedBox(height: 10),
-          Text(
-            movie.title,
-            maxLines: 1,
-            style: Theme.of(context).textTheme.bodySmall,
-          ),
-          Text(
-            '${((movie.rate * 10) ~/ 1).toString()}%',
-            style: Theme.of(context).textTheme.bodySmall,
-          ),
-        ],
+            const SizedBox(height: 5),
+            Text(
+              movie.title,
+              maxLines: 1,
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+            Text(
+              '${((movie.rate * 10) ~/ 1).toString()}%',
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+            const SizedBox(height: 5),
+          ],
+        ),
       ),
     );
   }
--- lib/components/movies_grid_view.dart
+++ lib/components/movies_grid_view.dart
@@ -4,21 +4,24 @@ import '../models/movie.dart';
 import 'movie_thumbnail.dart';
 
 Widget movieGridView(List<Movie> movies) {
-  return Padding(
-    padding: const EdgeInsets.only(
-      left: 16,
-      right: 16,
-      top: 16,
+  return
+      // Padding(
+      //   padding: const EdgeInsets.only(
+      //     left: 16,
+      //     right: 16,
+      //     top: 16,
+      //   ),
+      //   child:
+      GridView.builder(
+    itemCount: movies.length,
+    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      crossAxisCount: 2,
+      childAspectRatio: 220 / 330,
     ),
-    child: GridView.builder(
-      itemCount: movies.length,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-      ),
-      itemBuilder: (context, index) {
-        final movie = movies[index];
-        return MovieThumbnail(movie: movie);
-      },
-    ),
+    itemBuilder: (context, index) {
+      final movie = movies[index];
+      return MovieThumbnail(movie: movie);
+    },
   );
+  // );
 }
